2dcad6240cb9e140cd6188a2587731669a6e3fc4
eselbeus <eselbeus@gmail.com>
2019-01-21 02:51:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,26 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    contestant.each do |keys, data|
+      if contestant["status"] == "Winner"
+        first_name = contestant["name"].split(" ")
+          return first_name[0];
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_no, contestants|
+    contestants.each do |contestant|
+      contestant.each do |keys, values|
+        if occupation == values
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
