98062e60c113f8fb3573505d39f718c8238276a8
eselbeus <eselbeus@gmail.com>
2019-01-21 03:00:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -38,7 +38,14 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |keys, values|
+        if hometown == values
+          return contestant["occupation"]
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
